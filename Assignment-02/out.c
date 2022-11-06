bdfedb7c447672eece93aa19346715401188b1b2
LewisSarcasm <lewis36@hotmail.co.uk>
2022-11-06 14:16:17 +0000

    A02 done

---
+++ Assignment-02/out.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int i = 1;
+	float f = 0.1;
+	char c = 'c';
+	char string[] = "This is a string array not a single char";
+	
+	printf("%d is an integer!\n", i);
+	printf("%f is a float!\n", f);
+	printf("%c is a single char!\n", c);
+	printf("Many characters: %s\n", string);
+	
+	return 1;
+}
+
+
